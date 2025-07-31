67e972934e5f3933424f58fd6b1ba6f6a110b5f8
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-07-31 18:25:18 -0300

    Resolvendo bugs de string

--- PracaDaCiencia/PracaDaCiencia/settings.py
+++ PracaDaCiencia/PracaDaCiencia/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'PracaDaCiencia',
-    'apps.administrador'
+    'apps.administrador',
     'apps.visitantes'
 ]
 
--- PracaDaCiencia/apps/visitantes/apps.py
+++ PracaDaCiencia/apps/visitantes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VisitantesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'visitantes'
+    name = 'apps.visitantes'
